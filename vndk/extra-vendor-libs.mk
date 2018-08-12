7ae11257d5bdd3b942c0984d189444107b2e08b6
SGCMarkus <markusornik@gmail.com>
2018-08-12 23:29:59 +0200

    joan-common: treblelize

--- vndk/extra-vendor-libs.mk
+++ vndk/extra-vendor-libs.mk
@@ -1,4 +1,7 @@
 EXTRA_VENDOR_LIBRARIES := \
     android.hidl.base@1.0 \
     android.hidl.manager@1.0 \
-    vendor.display.config@1.0 
+    vendor.display.config@1.0 \
+    libsensor \
+    libpowermanager \
+    
