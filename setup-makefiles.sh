a8d145323d4c555a0e927550501db4700d8ac924
wills3gslide <44515711+wills3gslide@users.noreply.github.com>
2018-12-19 18:21:43 -0600

    Add files via upload

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -24,9 +24,9 @@ export INITIAL_COPYRIGHT_YEAR=2018
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-LINEAGE_ROOT="$MY_DIR"/../../..
+slim_ROOT="$MY_DIR"/../../..
 
-HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+HELPER="$slim_ROOT"/vendor/slim/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -34,10 +34,10 @@ fi
 . "$HELPER"
 
 # Initialize the helper for common device
-setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true
+setup_vendor "$DEVICE_COMMON" "$VENDOR" "$slim_ROOT" true
 
 # Copyright headers and common guards
-write_headers "h930 us998"
+write_headers "h932"
 
 write_makefiles "$MY_DIR"/proprietary-files.txt true
 
@@ -45,7 +45,7 @@ write_makefiles "$MY_DIR"/proprietary-files.txt true
 write_footers
 
 # Reinitialize the helper for device
-setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
+setup_vendor "$DEVICE" "$VENDOR" "$slim_ROOT"
 
 # Copyright headers and guards
 write_headers
